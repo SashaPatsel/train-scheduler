133003b8052cbcf1e95f8da06f6bdca927ecdcd9
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-03-02 17:17:59 -0800

    logic (mostly) done

--- assets/logic.js
+++ assets/logic.js
@@ -43,4 +43,48 @@ var database = firebase.database();
 
   });
 
- 
+  database.ref().on("child_added", function(snapshot) {
+
+      var newRow = $("<tr>");
+
+      var frequencyCheck = snapshot.val().frequency;
+      var firstTime = snapshot.val().time;
+
+      var firstMoment = moment(firstTime, "HH:mm");
+
+      var dayEnd = moment("23:59", "HH:mm");
+
+      var timesArray = [];
+
+      for (var i = firstMoment; i.isSameOrBefore(dayEnd); i.add(frequencyCheck, "minutes")) {
+
+          var times = i.format("HH:mm");
+
+          timesArray.push(times);
+      }
+
+      var currentTime = moment("20:52", "HH:mm");
+
+      var futureArray = [];
+
+      for (var i = 0; i < timesArray.length; i++) {
+
+          if (moment(timesArray[i], "HH:mm").isAfter(currentTime)) {
+              futureArray.push(timesArray[i]);
+          }
+      }
+
+      var nextTrain = futureArray[0];
+
+      var minutesAway = moment(nextTrain, "HH:mm").diff(currentTime, "minutes");
+
+      var showTime = moment(nextTrain, "HH:mm").format("h:mm a");
+
+      newRow.append("<td>" + snapshot.val().name + "</td>");
+      newRow.append("<td>" + snapshot.val().destination + "</td>");
+      newRow.append("<td>" + snapshot.val().frequency + "</td>");
+      newRow.append("<td>" + showTime + "</td>");
+      newRow.append("<td>" + minutesAway + "</td>");
+
+      $("#train-row").append(newRow);
+  });
